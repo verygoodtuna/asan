ace3070d63ea133485f35d260e5916e5a79f7ee9
verygoodtuna <kjy990504@gmail.com>
2023-08-09 18:16:10 +0900

    o3o

--- js/asan.js
+++ js/asan.js
@@ -3,4 +3,19 @@ $(function () {
         arrows: false,
         dots: true,
     })
+
+    $('.s_slide').slick ({
+        arrows: false,
+        slidesToShow: 5,
+    })
+
+    $('.tv_slide').slick ({
+        arrows: false,
+        slidesToShow: 2,
+    })
+
+    $('.imfor_slide').slick ({
+        arrows: false,
+        slidesToShow: 2,
+    })
 })
